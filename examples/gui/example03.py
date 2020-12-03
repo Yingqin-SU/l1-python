53419056b6db864076c670921668f80741d1cb58
Yingqin SU <swsuyingqin@gmail.com>
2020-12-03 18:40:42 +0100

    nouvelle modif

--- examples/gui/example03.py
+++ examples/gui/example03.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     x1 = CANVAS_WIDTH - 100
     y = CANVAS_HEIGHT / 2
     canvas.create_line(x0, y, x1, y)
-    canvas.create_oval(x0 - 50, y + 50, x0 + 50, y - 50)
+    canvas.create_oval((x0 - 50, y + 50), (x0 + 50, y - 50))
     canvas.create_oval(x1 - 50, y + 50, x1 + 50, y - 50)
     canvas.create_oval((x0 + x1) / 2 - 50, y + 50, (x0 + x1) / 2 + 50, y - 50)
     
